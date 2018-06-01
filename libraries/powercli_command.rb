15c5d6343fda34c443f8e29be455ed31696f971a
JJ Asghar <jj@chef.io>
2018-06-01 16:11:20 -0500

    Connection command working.

---
+++ libraries/powercli_command.rb
@@ -0,0 +1,47 @@
+class PowerCLICommand < Inspec.resource(1)
+  name 'powercli_command'
+
+  desc 'Run PowerCLI commands via InSpec'
+
+  example <<~EOX
+    # We expect TSM-SSH NOT to be running here.
+    cmd = 'Get-VMhost | Get-VMHostService | Where {$_.key -eq "TSM-SSH" -and $_.running -eq $False}'
+    conn_options = {
+      viserver: attribute('viserver', description: 'The server you want to connect to'),
+      username: attribute('username', description: 'Username to connect as'),
+      password: attribute('password', description: 'Password to connect with')
+    }
+    describe powercli_command(cmd, conn_options) do
+      its('exit_status') { should cmp 0 }
+      its('stdout') { should_not cmp '' }
+    end
+  EOX
+
+  attr_reader :stdout, :stderr, :exit_status
+
+  def initialize(cmd = '', conn_options = {})
+
+    @cmd = cmd
+    raise Inspec::Exceptions::ResourceFailed, 'Please update InSpec to newer than 2.1.43. Details: https://git.io/vhCNK' unless Inspec::VERSION >= '2.1.43'
+    raise Inspec::Exceptions::ResourceFailed, 'Please specify VIserver.' if conn_options[:viserver].nil?
+    raise Inspec::Exceptions::ResourceFailed, 'Please specify username.' if conn_options[:username].nil?
+    raise Inspec::Exceptions::ResourceFailed, 'Please specify password.' if conn_options[:password].nil?
+
+    viserver = conn_options[:viserver]
+    username = conn_options[:username]
+    password = conn_options[:password]
+
+    connect_command = "Connect-VIserver #{viserver} -User #{username} -Password #{password} | Out-Null;"
+    output = inspec.powershell(connect_command + cmd)
+
+    @stdout = output.stdout
+    @stderr = output.stderr
+    @exit_status = output.exit_status
+  end
+
+  def to_s
+    "powercli_command: #{@cmd}"
+  end
+
+
+end
